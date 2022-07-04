e6d18f94608cdc09627497ec793917345ac61ded
sagarbatra01 <87910501+sagarbatra01@users.noreply.github.com>
2022-07-04 05:15:24 +0200

    block highlights + sound

--- grid/constants.py
+++ grid/constants.py
@@ -1,13 +1,24 @@
-from pygame import FULLSCREEN
+import pygame
 
 # Window Settings
 APP_NAME = "Algorithm Visualizer"
-FULLSCREEN = True
-WIN_PIXEL_WIDTH = 1200 + FULLSCREEN * 600
-WIN_PIXEL_HEIGHT = 720 + FULLSCREEN * 200
+WIN_PIXEL_WIDTH = 1200
+WIN_PIXEL_HEIGHT = 720
 
 # Colors
 WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (199, 208, 238)
+
+# Sounds
+pygame.mixer.init()
+HOVER_SOUND = pygame.mixer.Sound(
+    "D:\GitProjects\\free_time_projects\grid\hover.wav")
+pygame.mixer.Sound.set_volume(HOVER_SOUND, 0.5)
 
 # Game Settings
 BLOCK_SIZE = 40
+BACKGROUND_COLOR = WHITE
+BLOCK_COLOR = BLACK
+HIGHLIGHT_COLOR = BLUE
+CURSOR_RECT_SIZE = 1
--- grid/main.py
+++ grid/main.py
@@ -3,17 +3,43 @@ from constants import *
 
 # Initialize the PyGame GUI.
 WIN = pygame.display.set_mode(
-    (WIN_PIXEL_WIDTH, WIN_PIXEL_HEIGHT), pygame.FULLSCREEN)
+    # (WIN_PIXEL_WIDTH, WIN_PIXEL_HEIGHT), pygame.FULLSCREEN)
+    (WIN_PIXEL_WIDTH, WIN_PIXEL_HEIGHT))
 pygame.display.set_caption(APP_NAME)
-WIN.fill(WHITE)
-pygame.display.flip()
+
+
+def drawGrid(old_selected):
+    for x in range(0, WIN_PIXEL_WIDTH, BLOCK_SIZE):
+        for y in range(0, WIN_PIXEL_HEIGHT, BLOCK_SIZE):
+            block = pygame.Rect(x, y, BLOCK_SIZE, BLOCK_SIZE)
+            pygame.draw.rect(WIN, BLOCK_COLOR, block, 1)
+            mouseX, mouseY = pygame.mouse.get_pos()
+            mouse_block = pygame.Rect(
+                mouseX, mouseY, CURSOR_RECT_SIZE, CURSOR_RECT_SIZE)
+            if block.contains(mouse_block):
+                selected = (x, y)
+                if selected != old_selected:
+                    pygame.mixer.Sound.play(HOVER_SOUND)
+                highlight_block = pygame.Rect(
+                    x + 1, y + 1, BLOCK_SIZE - 2, BLOCK_SIZE - 2)
+                pygame.draw.rect(WIN, HIGHLIGHT_COLOR,
+                                 highlight_block, 0)
+    return selected
 
 
 def main():
+    selected = (0, 0)
     while(True):
+        WIN.fill(BACKGROUND_COLOR)
+        selected = drawGrid(selected)
+        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    print(pygame.mouse.get_pos())
 
 
 if __name__ == "__main__":
