36dc5d1f5a27e2790498d242d80c6d6feddbf7fc
sagarbatra01 <87910501+sagarbatra01@users.noreply.github.com>
2022-07-05 01:55:02 +0200

    working bfs

---
+++ grid/block.py
@@ -0,0 +1,79 @@
+from tkinter import END
+from pygame import GL_GREEN_SIZE
+from constants import *
+
+
+class Block:
+    def __init__(self, row, col, size) -> None:
+        self.row = row
+        self.col = col
+        self.size = size
+        self.x = col * size
+        self.y = row * size
+        self.neighbors = set()
+        self.color = WHITE
+        self.visited = False
+        self.parent = None
+
+    def display(self, window):
+        pygame.draw.rect(window, self.color,
+                         (self.x, self.y, self.size, self.size))
+
+    def get_pos(self):
+        return (self.row, self.col)
+
+    def get_neighbors(self):
+        return self.neighbors
+
+    def set_pos(self, pos):
+        self.row, self.col = pos
+
+    def set_neighbors(self, neighbors):
+        self.neighbors = neighbors
+
+    def set_free(self):
+        self.color = FREE_COLOR
+
+    def is_free(self):
+        return self.color == FREE_COLOR
+
+    def set_wall(self):
+        self.color = WALL_COLOR
+
+    def is_wall(self):
+        return self.color == WALL_COLOR
+
+    def set_start(self):
+        self.color = START_COLOR
+
+    def is_start(self):
+        return self.color == START_COLOR
+
+    def set_end(self):
+        self.color = END_COLOR
+
+    def is_end(self):
+        return self.color == END_COLOR
+
+    def is_visited(self):
+        return self.visited
+
+    def set_visited(self, boolean):
+        self.visited = boolean
+        if not (self.is_start() or self.is_end() or self.is_wall()):
+            if boolean:
+                self.color = BLUE
+            else:
+                self.color = WHITE
+
+    def set_parent(self, parent):
+        self.parent = parent
+
+    def get_parent(self):
+        return self.parent
+
+    def set_path(self):
+        self.color = YELLOW
+
+    def is_path(self):
+        return self.color == YELLOW
--- grid/main.py
+++ grid/main.py
@@ -1,45 +1,186 @@
-import pygame
+from block import *
 from constants import *
-
-# Initialize the PyGame GUI.
-WIN = pygame.display.set_mode(
-    # (WIN_PIXEL_WIDTH, WIN_PIXEL_HEIGHT), pygame.FULLSCREEN)
-    (WIN_PIXEL_WIDTH, WIN_PIXEL_HEIGHT))
-pygame.display.set_caption(APP_NAME)
+import pygame
+from collections import deque
 
 
-def drawGrid(old_selected):
-    for x in range(0, WIN_PIXEL_WIDTH, BLOCK_SIZE):
-        for y in range(0, WIN_PIXEL_HEIGHT, BLOCK_SIZE):
-            block = pygame.Rect(x, y, BLOCK_SIZE, BLOCK_SIZE)
-            pygame.draw.rect(WIN, BLOCK_COLOR, block, 1)
-            mouseX, mouseY = pygame.mouse.get_pos()
-            mouse_block = pygame.Rect(
-                mouseX, mouseY, CURSOR_RECT_SIZE, CURSOR_RECT_SIZE)
-            if block.contains(mouse_block):
-                selected = (x, y)
-                if selected != old_selected:
-                    pygame.mixer.Sound.play(HOVER_SOUND)
-                highlight_block = pygame.Rect(
-                    x + 1, y + 1, BLOCK_SIZE - 2, BLOCK_SIZE - 2)
-                pygame.draw.rect(WIN, HIGHLIGHT_COLOR,
-                                 highlight_block, 0)
-    return selected
+def find_path(start, end):
+    if ALGO == "BFS":
+        q = deque()
+        q.appendleft(start)
+        start.set_visited(True)
+        start.parent = None
+        done = False
+        while q:
+            curr = q.pop()
+            curr
+            for n in curr.get_neighbors():
+                if not n.is_visited() and not n.is_wall():
+                    q.appendleft(n)
+                    n.set_visited(True)
+                    n.set_parent(curr)
+                    if n == end:
+                        done = True
+                        break
+            if done:
+                break
+        path = []
+        curr = end
+        while curr.get_parent() != None:
+            path.append(curr.get_pos())
+            curr = curr.get_parent()
+            if curr != start:
+                curr.set_path()
+
+        path.reverse()
+        return path
+
+
+def is_within_borders(coord, MATRIX):
+    return 0 <= coord[1] < len(MATRIX) and 0 <= coord[0] < len(MATRIX[0])
+
+
+def find_neighbors(MATRIX, r, c):
+    neighbors = set()
+    for offset in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+        coord = c + offset[0], r + offset[1]
+        if is_within_borders(coord, MATRIX):
+            neighbors.add(MATRIX[coord[1]][coord[0]])
+    return neighbors
+
+
+def add_neighbors(MATRIX):
+    for r in range(len(MATRIX)):
+        for c in range(len(MATRIX[0])):
+            MATRIX[r][c].set_neighbors(find_neighbors(MATRIX, r, c))
+
+
+def createMatrix(rows, cols, size):
+    matrix = [[] for _ in range(rows)]
+    for r in range(rows):
+        for c in range(cols):
+            block = Block(r, c, size)
+            matrix[r].append(block)
+    return matrix
+
+
+def drawGrid(window, rows, cols, size):
+    for r in range(rows):
+        pygame.draw.line(window, GRID_COLOR, (0, r * size),
+                         (cols * size, r * size))
+        for c in range(cols):
+            pygame.draw.line(window, GRID_COLOR, (c * size, 0),
+                             (c * size, rows * size))
+
+
+def clear_grid(MATRIX):
+    for row in MATRIX:
+        for block in row:
+            block.color = WHITE
+            block.set_visited(False)
+            block.set_parent(None)
+            block.neighbors.clear()
+    add_neighbors(MATRIX)
+
+
+def clear_path(MATRIX):
+    for row in MATRIX:
+        for block in row:
+            if block.is_path():
+                block.set_free()
+            block.set_visited(False)
+            block.set_parent(None)
+            block.neighbors.clear()
+    add_neighbors(MATRIX)
+
+
+def drawBlocks(matrix, window):
+    for row in matrix:
+        for block in row:
+            block.display(window)
+
+
+def get_block(matrix, position, size):
+    return matrix[position[1]//size][position[0]//size]
 
 
 def main():
-    selected = (0, 0)
+    pygame.init()
+    WINDOW = pygame.display.set_mode(
+        (WIN_PIXEL_WIDTH, WIN_PIXEL_HEIGHT))
+    pygame.display.set_caption(APP_NAME)
+    start, end = None, None
+    MATRIX = createMatrix(ROWS, COLS, BLOCK_SIZE)
+    add_neighbors(MATRIX)
+    running = False
     while(True):
-        WIN.fill(BACKGROUND_COLOR)
-        selected = drawGrid(selected)
+        if running:
+            clear_path(MATRIX)
+            find_path(start, end)
+        WINDOW.fill(BACKGROUND_COLOR)
+        drawBlocks(MATRIX, WINDOW)
+        drawGrid(WINDOW, ROWS, COLS, BLOCK_SIZE)
         pygame.display.flip()
+        block = get_block(
+            MATRIX, pygame.mouse.get_pos(), BLOCK_SIZE)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
+                exit(0)
 
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 1:
-                    print(pygame.mouse.get_pos())
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    if start and end:
+                        running = True
+
+                elif event.key == pygame.K_c:
+                    running = False
+                    clear_grid(MATRIX)
+                    start, end = None, None
+
+            if pygame.mouse.get_pressed()[0]:
+                if block.is_start():
+                    if not running:
+                        start = None
+                        block.set_wall()
+                elif block.is_end():
+                    if not running:
+                        end = None
+                        block.set_wall()
+                else:
+                    block.set_wall()
+                pygame.mixer.Sound.play(HOVER_SOUND)
+
+            elif pygame.mouse.get_pressed()[1]:
+                if block.is_start():
+                    start = None
+                if block.is_end():
+                    end = None
+                if start == None:
+                    start = block
+                    block.set_start()
+                elif end == None:
+                    end = block
+                    block.set_end()
+                else:
+                    start.set_free()
+                    start = block
+                    block.set_start()
+                pygame.mixer.Sound.play(HOVER_SOUND)
+
+            elif pygame.mouse.get_pressed()[2]:
+                if block.is_start():
+                    if not running:
+                        start = None
+                        block.set_free()
+                elif block.is_end():
+                    if not running:
+                        end = None
+                        block.set_free()
+                else:
+                    block.set_free()
+
+                pygame.mixer.Sound.play(HOVER_SOUND)
 
 
 if __name__ == "__main__":
